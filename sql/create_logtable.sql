58e2f4fcb832e8b78a2032919113e5d112bfa076
Dmitry Lukyanov <philothoph@gmail.com>
2025-07-11 10:15:57 +0400

    add procedure logs

--- sql/create_logtable.sql
+++ sql/create_logtable.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS "LOGS".etl_logs (
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP
 )
+
+CREATE TABLE IF NOT EXISTS "LOGS".procedure_logs (
+    id SERIAL PRIMARY KEY,
+    procedure_name VARCHAR(50) NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP
+)
