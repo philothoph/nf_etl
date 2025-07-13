36b4f168c8053ad692aff51339ec42ee8af5e7b7
Dmitry Lukyanov <philothoph@gmail.com>
2025-07-13 13:04:22 +0400

    add dm_f101_round_f table

--- sql/create_showcase_tables.sql
+++ sql/create_showcase_tables.sql
@@ -15,3 +15,24 @@ CREATE TABLE
         balance_out NUMERIC(23, 8),
         balance_out_rub NUMERIC(23, 8)
     );
+
+CREATE TABLE
+    IF NOT EXISTS "DM"."DM.DM_F101_ROUND_F" (
+        from_date DATE,
+        to_date DATE,
+        chapter CHAR(1),
+        ledger_account CHAR(5),
+        charasteristic CHAR(1),
+        balance_in_rub NUMERIC(23, 8),
+        balance_in_val NUMERIC(23, 8),
+        balance_in_total NUMERIC(23, 8),
+        turn_deb_rub NUMERIC(23, 8),
+        turn_deb_val NUMERIC(23, 8),
+        turn_deb_total NUMERIC(23, 8),
+        turn_cre_rub NUMERIC(23, 8),
+        turn_cre_val NUMERIC(23, 8),
+        turn_cre_total NUMERIC(23, 8),
+        balance_out_rub NUMERIC(23, 8),
+        balance_out_val NUMERIC(23, 8),
+        balance_out_total NUMERIC(23, 8)
+    )
